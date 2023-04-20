d87a9b9d067b1d6811ed1b908ab9d65785659288
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2023-04-20 22:04:55 +0530

    Setting Initial Project

---
+++ xcavate/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="flex justify-center items-center h-[100vh] border">
+        <h1 className="text-blue-500 text-5xl font-bold ">Hello World , <br/> Welcome's to Xcavate</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
