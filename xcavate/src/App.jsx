bf8f0f9b90ae3402b1688088d5562501d1dd849d
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2023-04-27 11:17:16 +0530

    Profile Form Page

--- xcavate/src/App.jsx
+++ xcavate/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Profile from "./Components/Profile";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <>  
+      <Profile/>
       <div className="flex justify-center items-center h-[100vh] border">
         <h1 className="text-blue-500 text-5xl font-bold ">Hello World , <br/> Welcome's to Xcavate</h1>
       </div>
