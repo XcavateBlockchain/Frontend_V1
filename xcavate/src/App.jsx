23962d66d6760fa3b7e956ad9575a93a4b942fe5
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2023-06-01 00:18:50 +0530

    Adding

--- xcavate/src/App.jsx
+++ xcavate/src/App.jsx
@@ -14,8 +14,8 @@ function App() {
   return (
     <>
       <Navbar />
-      <RealEstate/>
-      {/* <Home /> */}
+      {/* <RealEstate/> */}
+      <Home />
       {/* <Marketplace/>
       <Property /> */}
       {/* <Profile/> */}
