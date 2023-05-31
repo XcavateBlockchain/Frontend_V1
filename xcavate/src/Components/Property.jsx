43e1cd90a53e624775685acb2bca1ad38ba960c7
Yusuf Bashir <yusuf.bashir@halogen-group.com>
2023-05-31 21:31:51 +0100

    latest

--- xcavate/src/Components/Property.jsx
+++ xcavate/src/Components/Property.jsx
@@ -4,16 +4,16 @@ const Property = () => {
   return (
     <>
       <div className="container m-auto flex justify-center items-center h-[100%]">
-        <div class="border-b border-gray-200 dark:border-gray-700">
-          <ul class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
-            <li class="mr-2">
+        <div className="border-b border-gray-200 dark:border-gray-700">
+          <ul className="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+            <li className="mr-2">
               <a
                 href="#"
-                class="inline-flex p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"
+                className="inline-flex p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"
               >
                 <svg
                   aria-hidden="true"
-                  class="w-5 h-5 mr-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300"
+                  className="w-5 h-5 mr-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
@@ -27,15 +27,15 @@ const Property = () => {
                 Property Information
               </a>
             </li>
-            <li class="mr-2">
+            <li className="mr-2">
               <a
                 href="#"
-                class="inline-flex p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500 group"
+                className="inline-flex p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500 group"
                 aria-current="page"
               >
                 <svg
                   aria-hidden="true"
-                  class="w-5 h-5 mr-2 text-blue-600 dark:text-blue-500"
+                  className="w-5 h-5 mr-2 text-blue-600 dark:text-blue-500"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
@@ -46,14 +46,14 @@ const Property = () => {
               </a>
             </li>
            
-            <li class="mr-2">
+            <li className="mr-2">
               <a
                 href="#"
-                class="inline-flex p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"
+                className="inline-flex p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"
               >
                 <svg
                   aria-hidden="true"
-                  class="w-5 h-5 mr-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300"
+                  className="w-5 h-5 mr-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
