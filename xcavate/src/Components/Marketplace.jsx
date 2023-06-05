5cc1c2eea5f6c7246574ac4119a28f8e2968b047
Xcavate <help@xcavate.io>
2023-06-05 16:33:18 +0100

    Remove the header image and logo

--- xcavate/src/Components/Marketplace.jsx
+++ xcavate/src/Components/Marketplace.jsx
@@ -4,14 +4,11 @@ import AssetCard from "./AssetCard";
 const Marketplace = () => {
   return (
     <>
-      <div className="container banner h-[14rem] border w-[100%] relative p-4 mx-auto mb-5">
-      <div className="w-[120px] h-[120px] left-10 p-1 absolute bottom-[-50px] rounded-full  profile-banner"/>
-      </div>
-      <div className="px-4 py-10 grid grid-cols-3 gap-2 container mx-auto">
+      <center><div className="px-4 py-10 grid grid-cols-3 gap-2 container mx-auto">
        {data.map((item)=>{
          return <AssetCard item={item} key={item._id} />
        })}
-      </div>
+      </div></center>
     </>
   );
 };
