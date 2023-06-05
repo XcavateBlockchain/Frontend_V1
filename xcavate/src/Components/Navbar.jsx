4cf5cc44ef91a301953d07d689763ebeff44b684
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2023-06-05 15:14:50 +0530

    Update Home Page

--- xcavate/src/Components/Navbar.jsx
+++ xcavate/src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <>
       <nav className="bg-white  shadow-xl fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600 main-color">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://flowbite.com/" className="flex items-center">
+          <a href="/" className="flex items-center">
             <img
               src={logo}
               className="h-10 mr-3"
@@ -17,7 +17,7 @@ const Navbar = () => {
           <div className="flex md:order-2">
             <button
               type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className=" connect-wallet-button rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0"
             >
               Connect Wallet
             </button>
