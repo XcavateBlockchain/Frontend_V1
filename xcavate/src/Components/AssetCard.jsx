d7216901bb4e56318182964a73f2a67f9146270e
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2023-06-05 23:39:21 +0530

    Update Market place

--- xcavate/src/Components/AssetCard.jsx
+++ xcavate/src/Components/AssetCard.jsx
@@ -8,8 +8,8 @@ import {GiBathtub} from "react-icons/gi";
 const AssetCard = ({ item, isHome, company }) => {
   return (
     <>
-      <div className="max-w-sm mt-2 bg-white relative h-auto  asset-card rounded-lg shadow-2xl">
-        <div className="w-[100%] overflow-hidden h-[58%]">
+      <div className={`max-w-sm mt-2 bg-white relative ${!isHome ?  "h-[25rem]" : "h-[90%]" } asset-card rounded-lg shadow-2xl`}  >
+        <div className={`w-[100%] overflow-hidden ${!isHome ?  "h-[48%]" : "h-[58%]" }`}>
           <img className="rounded-t-lg" src={item.photo2} alt="hello" />
         </div>
        {!isHome ?  <div className="p-3 absolute top-0">
@@ -21,20 +21,20 @@ const AssetCard = ({ item, isHome, company }) => {
             <BsHeart size={20} color="white"/>
           </span>
         </div>}
-        <div className="p-5">
+        <div className="p-5 ">
           <div className="flex justify-between">
             <h5 className="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-black">
               {item.title}
             </h5>
-            <h5 className="mb-2 text-1xl font-bold tracking-tight text-blue-400 dark:text-blue-400">
-              $ {item.listingPrice}
+            <h5 className="mb-2 text-1xl font-bold tracking-tight text-black-400 dark:text-black-400">
+            £ {item.listingPrice}
             </h5>
           </div>
-          {!isHome ? (
+          {/* {!isHome ? (
             <h3 className="text-black mt-1 mb-1 capitalize">
               {item.description}
             </h3>
-          ) : null}
+          ) : null} */}
           <div className="flex">
             <a className="flex" href={item.googleMapCode}>
               <HiOutlineLocationMarker className="text-blue-400" size={22} />
@@ -52,29 +52,24 @@ const AssetCard = ({ item, isHome, company }) => {
             </div>
             <img
               class="w-12 h-12 bg-black rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
-              src={company}
+              src={company ? company : item.company}
               alt="Bordered avatar"
             />
           </div>
           {!isHome ? (
             <>
               <div className="mt-5 flex justify-between">
-                <button className="inline-flex mint-now-button  items-center px-3 py-2 text-sm text-center text-black rounded-lg ">
-                  Buy Now
+                <button className="inline-flex button items-center px-5 py-1 text-sm text-center text-white rounded-lg ">
+                 Buy
                 </button>
-                <div className="flex">
-                  <a
+                <div className="flex w-[75%]">
+                  <button
                     href={item.floorPlan}
-                    className="inline-flex view-details-button border items-center px-3 py-2 text-sm text-center text-black rounded-lg"
+                    className="block w-full border border-[#2F8BB2] text-[#2f8bb2] items-center px-3 py-2 text-sm text-center rounded-lg"
                   >
-                    Floor Plan
-                  </a>
-                  <a
-                    href={item.salesAgreement}
-                    className="inline-flex mx-3 view-details-button border items-center px-3 py-2 text-sm text-center text-black rounded-lg"
-                  >
-                    Sales Agreement
-                  </a>
+                    Details
+                  </button>
+                
                 </div>
               </div>
             </>
--- xcavate/src/Components/Marketplace.jsx
+++ xcavate/src/Components/Marketplace.jsx
@@ -4,15 +4,17 @@ import AssetCard from "./AssetCard";
 const Marketplace = () => {
   return (
     <>
-      <div> {
-        padding-top: 50px;
-    }
+      {/* <div className="container banner h-[14rem] border w-[100%] relative p-4 mx-auto mb-5">
+      <div className="w-[120px] h-[120px] left-10 p-1 absolute bottom-[-50px] rounded-full  profile-banner"/>
+      </div> */}
+      <div className="text-center w-full mt-40">
+        <h2 className="text-3xl text-black">Explore The Marketplace</h2>
       </div>
-      <center><div className="px-4 py-10 grid grid-cols-3 gap-2 container mx-auto">
+      <div className="px-4  py-10 grid grid-cols-3 gap-2 container mx-auto">
        {data.map((item)=>{
          return <AssetCard item={item} key={item._id} />
        })}
-      </div></center>
+      </div>
     </>
   );
 };
