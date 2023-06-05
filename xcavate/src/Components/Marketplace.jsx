ab49b6aad6d4e0b8d3fec4fdf728481c4a275d0a
Xcavate <help@xcavate.io>
2023-06-05 16:38:49 +0100

    Add top padding

--- xcavate/src/Components/Marketplace.jsx
+++ xcavate/src/Components/Marketplace.jsx
@@ -4,6 +4,10 @@ import AssetCard from "./AssetCard";
 const Marketplace = () => {
   return (
     <>
+      <div> {
+        padding-top: 50px;
+    }
+      </div>
       <center><div className="px-4 py-10 grid grid-cols-3 gap-2 container mx-auto">
        {data.map((item)=>{
          return <AssetCard item={item} key={item._id} />
